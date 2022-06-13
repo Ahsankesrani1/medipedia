dd96f58403d7292382ce865442910f13aee95b11
Ahsankesrani1 <ahsankesrana7@gmail.com>
2022-06-13 21:46:08 +0500

    laravall install

---
+++ resources/views/pages/info.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.master')
+@section('content')
+
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex justify-content-center align-items-center">
+    <div class="container position-relative">
+      <p class="text-white text-center">Truly know your Medicine before you take them.</p>
+      <input class="medipedia-search offset-md-4 col-md-4 offset-md-4" placeholder="medipedia search" type="search" />
+    </div>
+  </section><!-- End Hero -->
+
+
+@endsection
